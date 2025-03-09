2ede7d8126032185e9fd719ef2fac3a4e84ce466
Hossein Mirhosseini <h.mirhosseini@hzdr.de>
2025-03-09 10:54:31 +0100

    step2

--- aiida_datagen/workflows/step2.py
+++ aiida_datagen/workflows/step2.py
@@ -32,32 +32,21 @@ def collect_random_structures(outfile):
             random_bulk_structures.append(a_pymatgen_structure.as_dict())
     return nat, random_bulk_structures
 
-def store_step2_results():
-    todump_dict = defaultdict(list)
-    wf_step2_group = Group.collection.get(label='wf_step2')
+def store_step2_results(results_dict):
     random_structures_group = Group.collection.get(label='random_structures')
     random_structures_group.clear()
-    for a_wf_node in wf_step2_group.nodes:
-        a_node = a_wf_node.called[-1]
-        if not a_node.is_finished_ok:
-            continue
-        output_folder = a_node.outputs.retrieved
-        with output_folder.open('posout.yaml', 'rb') as fhandle:
-            nat, random_bulk_structures = collect_random_structures(fhandle)
-        if nat and random_bulk_structures:
-            todump_dict[str(nat)].extend(random_bulk_structures)
-    for a_key in todump_dict.keys():
-        a_node = Dict({a_key: todump_dict[a_key]}).store()
+    for a_key in results_dict.keys():
+        a_node = Dict({a_key: results_dict[a_key]}).store()
         a_node.label = a_key
         random_structures_group.add_nodes(a_node)
-        log_write(f'{len(todump_dict[a_key])} random bulk structures with {a_key} atoms are generated'+'\n')
+        log_write(f'{len(results_dict[a_key])} random bulk structures with {a_key} atoms are generated'+'\n')
 
-def generate_random_bulk(ini_spg, elements, n_element, vpas, n):
+def generate_random_bulk(ini_spg, elements, n_element, vpas, attempts):
     min_d_prefactor = min(0.90, inputs['min_distance_prefactor'])
     random_structures = []
     failed_attempts = 0
     spg = ini_spg
-    while len(random_structures) < n:
+    while len(random_structures) < attempts:
         vol = uniform(min(vpas), max(vpas)) * sum(n_element)
         try:
             r_b_3d = random_crystal_3d(3, spg, elements, n_element, vol)
@@ -76,10 +65,9 @@ def generate_random_bulk(ini_spg, elements, n_element, vpas, n):
             continue
         pymatgen_structure = Structure(lattice, species, coords)
         if is_structure_valid(pymatgen_structure, False, min_d_prefactor, True, False, False)[0]:
-            random_structures.append(pymatgen_structure)
+            random_structures.append(pymatgen_structure.as_dict())
             failed_attempts = 0
             spg = ini_spg
-            print(pymatgen_structure)
         else:
             failed_attempts = failed_attempts + 1
         if failed_attempts > 5:
@@ -87,7 +75,7 @@ def generate_random_bulk(ini_spg, elements, n_element, vpas, n):
     return random_structures
 
 def step2_pyxtal(composition_list):
-    todump_dict = defaultdict(list)
+    results_dict = defaultdict(list)
     vpas = []
     known_structures_group = Group.collection.get(label='known_structures')
     for a_node in known_structures_group.nodes:
@@ -105,12 +93,15 @@ def step2_pyxtal(composition_list):
             random_bulk_structures = []
             n_element = [n_el*int(a_n_a/sum(n_elmnt)) for n_el in n_elmnt]
             for spg in range(1, 231):
-                random_bulk_structures.append(generate_random_bulk(spg, elements, n_element, vpas, attempts))
-            todump_dict[str(a_n_a)].extend(random_bulk_structures)
-    return todump_dict
+                random_bulk_structures.extend(generate_random_bulk(spg, elements, n_element, vpas, attempts))
+            results_dict[str(a_n_a)].extend(random_bulk_structures)
+    # store resutls
+    store_step2_results(results_dict)
 
 def step2_flame(composition_list):
     data_dict = {}
+    results_dict = defaultdict(list)
+
     for a_comp in composition_list:
         allowed_n_atom = get_allowed_n_atom_for_compositions([a_comp])
         attempts = max(round(5*(inputs['number_of_bulk_structures']/len(inputs['bulk_number_of_atoms']))/230), 1)
@@ -125,8 +116,19 @@ def step2_flame(composition_list):
         if controller.num_to_run > 0:
             controller.submit_new_batch(dry_run=False)
         sleep(60)
+    # extract data
+    wf_step2_group = Group.collection.get(label='wf_step2')
+    for a_wf_node in wf_step2_group.nodes:
+        a_node = a_wf_node.called[-1]
+        if not a_node.is_finished_ok:
+            continue
+        output_folder = a_node.outputs.retrieved
+        with output_folder.open('posout.yaml', 'rb') as fhandle:
+            nat, random_bulk_structures = collect_random_structures(fhandle)
+        if nat and random_bulk_structures:
+            results_dict[str(nat)].extend(random_bulk_structures)
     # store resutls
-    store_step2_results()
+    store_step2_results(results_dict)
 
 def step_2():
     """ Step 2
@@ -135,7 +137,6 @@ def step_2():
         log_write("---------------------------------------------------------------------------------------------------"+'\n')
         log_write('STEP 2'+'\n')
         log_write(f'start time: {get_time()}'+'\n')
-        log_write('random structure generation with gensymcrys'+'\n')
         # check
         previous_run_exist_check()
         group_is_empty_check('wf_step2')
@@ -148,8 +149,10 @@ def step_2():
             log_write('>>> ERROR: data for step 2 is not complete'+'\n')
             sys.exit()
         if inputs['random_structure_generator'] in ['PyXtal', 'pyxtal']:
+            log_write('random structure generation with pyxtal'+'\n')
             step2_pyxtal(composition_list)
         else:
+            log_write('random structure generation with gensymcrys'+'\n')
             step2_flame(composition_list)
         log_write('STEP 2 ended'+'\n')
         log_write(f'end time: {get_time()}'+'\n')
